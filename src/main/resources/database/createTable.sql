f2cead974bad14e01f559b508a0e3101facaba52
brainrtp <brainrtp@yandex.ru>
2021-11-15 22:40:06 +0500

    Change table name to correct

--- src/main/resources/database/createTable.sql
+++ src/main/resources/database/createTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS eastereggs
 (
     id       INT(11) AUTO_INCREMENT PRIMARY KEY,
-    player   VARCHAR(40),
+    player_uuid   VARCHAR(40),
     category VARCHAR(32),
     egg_id INT(11)
 );
